c3f02db436da68ba9100ea5a8ca4b5134021c460
DiegoX <diegomenendez17@gmail.com>
2023-06-02 10:31:18 -0600

    Primer commit del proyecto con los paquetes necesarios para el funcionamiento

---
+++ src/test/java/sv/com/ApiCarritoCompras/ApiCarritoComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.com.ApiCarritoCompras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCarritoComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

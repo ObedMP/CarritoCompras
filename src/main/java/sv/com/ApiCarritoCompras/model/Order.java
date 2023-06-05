de975eab1b264015754792a552287fd5a5e6bc88
DiegoX <diegomenendez17@gmail.com>
2023-06-05 13:42:29 -0600

    terminando la capa model

--- src/main/java/sv/com/ApiCarritoCompras/model/Order.java
+++ src/main/java/sv/com/ApiCarritoCompras/model/Order.java
@@ -1,4 +1,37 @@
 package sv.com.ApiCarritoCompras.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity(name = "Order")
+@Table(name = "orders")
+@Data @AllArgsConstructor
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idorder")
+    private Integer idOrder;
+
+    @ManyToOne
+    @JoinColumn(name = "iduser")
+    private User user;
+
+    @Column(name = "order_status")
+    private String orderStat;
+
+    @Column(name = "totalm")
+    private double totalamount;
+
+    public Order(int i, User user, String orderStat, double totalamount) {
+
+    }
+
+    public Order() {
+    }
+
+    public Integer getIdOrder() {
+        return idOrder;
+    }
 }
--- src/main/java/sv/com/ApiCarritoCompras/model/User.java
+++ src/main/java/sv/com/ApiCarritoCompras/model/User.java
@@ -1,4 +1,17 @@
 package sv.com.ApiCarritoCompras.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+@Entity(name = "User")
+@Table(name = "users")
+@Data
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column()
 }
